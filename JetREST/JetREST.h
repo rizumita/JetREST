d8b6e0fb05a32a0eb44918b742c574d8ca0571b1
Ryoichi Izumita <r.izumita@caph.jp>
2015-09-25 23:31:56 +0900

    Initial Commit

---
+++ JetREST/JetREST.h
@@ -0,0 +1,19 @@
+//
+//  JetREST.h
+//  JetREST
+//
+//  Created by 和泉田領一 on 2015/09/25.
+//  Copyright © 2015年 CAPH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JetREST.
+FOUNDATION_EXPORT double JetRESTVersionNumber;
+
+//! Project version string for JetREST.
+FOUNDATION_EXPORT const unsigned char JetRESTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JetREST/PublicHeader.h>
+
+
